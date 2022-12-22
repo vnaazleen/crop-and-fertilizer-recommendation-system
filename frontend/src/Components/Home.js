e8352de826b3e3e28eb5b8aed4dc011d3aa04ed0
Vaseem Naazleen <shaikvaseemnaazleen@gmail.com>
2022-12-22 18:40:22 +0530

    Created Home Page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,5 +1,34 @@
+import './Home.css'
+
+import { Button } from '@mui/material';
+
 export default function Home() {
     return (
-        <div>Crop &amp; Fertilizer Recommendation System</div>
+        <div className="Home">
+            <div className='Buttons'>
+            <Button 
+             sx={{ color: 'white', 
+                   borderColor: 'white',
+                   "&:hover": { borderColor: "green", backgroundColor: "green" } 
+                }}
+             className="Btn1" 
+             variant="outlined"
+             href="/crop-recommender"
+            >
+                Crop Recommender
+            </Button>
+                &nbsp; &nbsp;
+            <Button 
+             sx={{ color: 'white', 
+                    borderColor: 'white',
+                    "&:hover": { borderColor: "green", backgroundColor: "green" } 
+                }}
+             className="Btn1" 
+             variant="outlined"
+            >
+                Fertilizer Recommender
+            </Button>
+            </div>
+        </div>
     )
 }
