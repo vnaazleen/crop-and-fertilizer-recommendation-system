dcde2e87e53a0bfdc3e038416fd9bcb7ed070fd5
Vaseem Naazleen <shaikvaseemnaazleen@gmail.com>
2023-01-05 22:00:13 +0530

    Integrate Geolocation & State-City in Crop Recommender

--- frontend/src/Components/CropRecommender/CropRecommender.js
+++ frontend/src/Components/CropRecommender/CropRecommender.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Alert, Button, TextField, Box } from '@mui/material';
+import { State, City }  from 'country-state-city';
+import { Alert, Button, TextField, Box, Radio, RadioGroup, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 
 import './CropRecommender.css';
 
@@ -10,16 +11,40 @@ const URL = "http://127.0.0.1:5000/crops"
 export default function CropRecommender() {
 
     const [crop, setCrop] = useState("")
+    const [chooseStateCity, setChooseStateCity] = useState(false)
 
-    const handleSubmit = (event) => {
+    const country = "India"
+    const [state, setState] = useState("")
+    const [city, setCity] = useState("")
+
+    const [states, setStates] = useState([])
+    const [cities, setCities] = useState([])
+
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        setStates(State.getStatesOfCountry("IN"))
+    }, [country])
+
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!chooseStateCity) {
+            await getGeoLocation()
+        }
+
+        await fetchTempertureAndHumidity()
+
         const fromElements = event.target.elements;
         const formInput = {
             "N": fromElements["N"].value,
             "P": fromElements["P"].value,
             "K": fromElements["K"].value,
-            "temperature": fromElements["temperature"].value,
-            "humidity": fromElements["humidity"].value,
+            "temperature": data.main.temp,
+            "humidity": data.main.humidity,
             "pH": fromElements["pH"].value,
             "rainfall": fromElements["rainfall"].value
         }
@@ -27,11 +52,77 @@ export default function CropRecommender() {
         console.log(formInput)
 
         axios.post(URL, formInput)
-        .then((response) => {
+        .then(async (response) => {
             console.log(response.data)
-            setCrop(response.data.crop)
+            await setCrop(response.data.crop)
         })
         .catch((error) => alert(error))
+    }
+
+    const handleLocationInfoChange = (event) => {
+        if (event.target.value === "geolocation") {
+            setChooseStateCity(false)
+        } else {
+            setChooseStateCity(true)
+        }
+
+        console.log(event.target.value)
+    }
+
+    const fetchTempertureAndHumidity = async () => {
+        await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            .then(res => res.json())
+            .then(result => {
+                setData(result)
+            })
+    }
+
+    const updateState = (latitude, longitude) => {
+
+        console.log("Latitude is :", latitude)
+        console.log("Longitude is :", longitude)
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+    }
+
+    const options = {
+        enableHighAccuracy: false,
+        timeout: 500000,
+        maximumAge: Infinity
+    }
+
+    const successCallback = (position) => {
+        updateState(position.coords.latitude, position.coords.longitude)
+    }
+    
+    const errorCallback = (error) => {
+        console.log(error)
+    }
+
+    const getGeoLocation = () => {
+        console.log("Getting geo-location...")
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+    }
+
+    const handleStateChange = (event) => {
+        setState(event.target.value)
+
+        states.forEach((state) => {
+            if (state.name === event.target.value) {
+                setCities(City.getCitiesOfState("IN", state.isoCode))
+            }
+        })
+    }
+
+    const handleCityChange = (event) => {
+        setCity(event.target.value)
+
+        cities.forEach((c) => {
+            if (c.name === event.target.value) {
+                updateState(c.latitude, c.longitude)
+            }
+        })
     }
 
     return (
@@ -43,7 +134,7 @@ export default function CropRecommender() {
                 }}
               autoComplete="off"
               onSubmit={e => handleSubmit(e)}
-              className="form"
+              className="cropForm"
             >
                 <div>
                     <center>
@@ -72,7 +163,7 @@ export default function CropRecommender() {
                             type="number"
                         />
                         <br />
-                        <TextField
+                        {/* <TextField
                             required
                             id="temperature"
                             name="temperature"
@@ -93,6 +184,65 @@ export default function CropRecommender() {
                                 step: "any"
                             }}
                         />
+                        <br /> */}
+
+                        <FormControl sx={{m:1, width: '25ch'}}>
+                            <FormLabel id="location">Location</FormLabel>
+                            <RadioGroup
+                                defaultValue="geolocation"
+                                name="radio-buttons-group"
+                                onChange={handleLocationInfoChange}
+                                required
+                            >
+                                <FormControlLabel value="geolocation" control={<Radio />} label="Get via Geo-location" />
+                                <FormControlLabel value="enter" control={<Radio />} label="Enter Manually" />
+                            </RadioGroup>
+                        </FormControl>
+
+                        <br/>
+                        
+                        {
+                            chooseStateCity 
+
+                            &&
+                            <div>
+                                <FormControl sx={{m:1, width: '25ch'}}>
+                                    <InputLabel id="state">State</InputLabel>
+                                    <Select
+                                    id="state-select"
+                                    value={state}
+                                    label="State"
+                                    onChange={handleStateChange}
+                                    >
+                                    {
+                                        states.map((s) => {
+                                            return <MenuItem value={s.name} key={s.name}>{s.name}</MenuItem>
+                                        })
+                                    }
+                                    </Select>
+                                </FormControl>
+                            
+                            <br/>
+                            
+                                <FormControl sx={{m:1, width: '25ch'}}>
+                                    <InputLabel id="city">City</InputLabel>
+                                    <Select
+                                    id="city-select"
+                                    value={city}
+                                    label="City"
+                                    onChange={handleCityChange}
+                                    >
+                                    {
+                                        cities.map((c) => {
+                                            return <MenuItem value={c.name} key={c.name}>{c.name}</MenuItem>
+                                        })
+                                    }
+                                    </Select>
+                                </FormControl>
+                            </div>
+
+                        }
+
                         <br />
                         <TextField
                             required
